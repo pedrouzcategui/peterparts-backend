fa1b88f1eac2af507cb1a024b326efc7cb7944b5
Pedro <pedrouzcategui0103@gmail.com>
2026-02-28 22:47:07 -0400

    feat(auth): integrate auth routes into main app

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
 import express, { type Request, type Response } from "express";
-import { productRouter } from "@routers/index";
+import cookieParser from "cookie-parser";
+import { productRouter, authRouter } from "@routers/index";
 import { checkDatabaseConnection } from "@db/index";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
 app.use("/products", productRouter);
+app.use("/auth", authRouter);
 
 app.listen(port, async () => {
   await checkDatabaseConnection();
